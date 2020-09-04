9efbcf2d00fc941c356fc6b829e0ac9a5800ac74
Krsnananda <krsnananda@outlook.com>
2020-09-04 08:36:53 -0300

    - Adiciona Short Bubble

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,3 +9,21 @@ def bubbleSort(list):
 list = [4,3,78,2,0,2]
 bubbleSort(list)
 print(list)
+
+# Short Bubble para com as trocas no momento que descobre que a lista está ordenada, sem realizar mais trocas desnecessárias
+def shortBubble(list):
+  exchanges = True
+  passnum = len(list) - 1
+  while passnum > 0 and exchanges:
+    exchanges = False
+    for i in range(passnum):
+      if list[i] > list[i+1]:
+        exchanges = True
+        temp = list[i]
+        list[i] = list[i+1]
+        list[i+1] = temp
+    passnum = passnum - 1
+
+list = [4,3,78,2,0,2]
+shortBubble(list)
+print(list)
